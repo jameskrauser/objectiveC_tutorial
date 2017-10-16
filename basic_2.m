6dfffb7728bc041213ffd3dca2b9e68ce3a12bbf
jameskrauser <jameskrauser@gmail.com>
2017-10-16 12:17:56 +0800

    objectiveC basic struct setter/getter
    objectiveC basic struct setter/getter

---
+++ basic_2.m
@@ -0,0 +1,47 @@
+//
+//  main.m
+//  objectiveC_1_1
+//
+//  Created by jameskrauser on 2017/10/16.
+//  Copyright © 2017年 jameskrauser. All rights reserved.
+//  you need to write setter/getter
+
+#import "Car.h"
+
+@implementation Car
+
+// -() instance method
+-(int) price
+{
+    return price;
+}
+
+-(void) setPrice:(int)inputp
+{
+    price = inputp;
+}
+
+// +()  calss method
++(void)printCarInfo
+{
+    NSLog(@"Hello, this is car info");
+}
+@end
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        
+        //class method call dont need generate instance
+        [Car printCarInfo];
+        
+        
+        //instance method
+        Car *ptr = [ [Car alloc] init ];
+        [ptr setPrice:20];
+        NSLog(@"the price of car is %i " , [ptr price]);
+      
+        
+        NSLog(@" --- finish --- ");
+    }
+    return 0;
+}
