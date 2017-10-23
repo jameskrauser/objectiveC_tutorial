f84cfe0a205ce4672a20da3eed06322ba44f7e13
jameskrauser <jameskrauser@gmail.com>
2017-10-23 14:32:30 +0800

    Create category_a.h

---
+++ category_a.h
@@ -0,0 +1,15 @@
+//
+//  category_a.h
+//  category1
+//
+//  Created by jameskrauser on 2017/10/23.
+//  Copyright © 2017年 jameskrauser. All rights reserved.
+//
+
+#ifndef category_a_h
+#define category_a_h
+
+@interface NSString (NSStringCategory)
+-(void) ShowJamesName;
+@end
+#endif /* category_a_h */
