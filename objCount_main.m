329c5c0fb61c1b8398d4f37abeb8374e15f0e7f3
jameskrauser <jameskrauser@gmail.com>
2017-10-26 17:55:01 +0800

    Update objCount_main.m

--- objCount_main.m
+++ objCount_main.m
@@ -13,12 +13,15 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         myobj *aobj = [[myobj alloc] init ];
         NSLog(@"car count = %d" , [aobj getCounts] );
+        NSLog(@"name is %@" , [aobj getProductName] );
         
         myobj *bobj = [[myobj alloc] init];
         NSLog(@"car count = %d" , [bobj getCounts ]);
+        NSLog(@"name is %@" , [aobj getProductName] );
 
         myobj *cobj = [[myobj alloc] init ];
         NSLog(@"car count = %d" , [cobj getCounts]);
+        NSLog(@"name is %@" , [aobj getProductName] );
         bobj = NULL;
         
         NSLog(@"for now car count = %d" ,[myobj Counts] );
